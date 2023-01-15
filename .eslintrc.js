8dd6155c94a573144851715b32d0d7ade7a8a97c
rmanaem <rmanaem@protonmail.ch>
2023-01-15 11:58:29 -0500

    Added eslint as pre-commit hook to husky
    - Auto fixed all files with linter
    - Removed NuxtLogo.vue and Tutorial.vue files due to pass linter check
    - Modified index.vue to reflect changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
     'plugin:nuxt/recommended',
     'plugin:vue/recommended',
     // https://www.npmjs.com/package/eslint-config-airbnb-base
-    'airbnb-base'
+    'airbnb-base',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
-}
+  rules: {},
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <Tutorial />
+  <div>Query Tool</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: 'IndexPage',
+};
 </script>
