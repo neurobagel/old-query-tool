1e201ac8c4f6d884d202b4e06b32b11358b095e3
rmanaem <rmanaem@protonmail.ch>
2023-01-18 12:42:11 -0500

    Implemented age lower and upper UI elements in QueryForm.vue component

---
+++ components/QueryForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-row class="mx-4">
+    <b-col cols="2">
+      <b-row>
+        <h2>Query fields</h2>
+      </b-row>
+      <b-row>
+        <b-form>
+          <b-form-row>
+            <b-form-group class="col-md-6">
+              <div class="mb-3">
+                <label class="form-label">Age lower</label>
+                <b-form-input
+                  type="number"
+                  name="age_lower"
+                  step="any"
+                  class="numberinput form-control"
+                />
+              </div>
+            </b-form-group>
+            <b-form-group class="col-md-6">
+              <div class="mb-3">
+                <label class="form-label">Age upper</label>
+                <b-form-input
+                  type="number"
+                  name="age_upper"
+                  step="any"
+                  class="numberinput form-control"
+                />
+              </div>
+            </b-form-group>
+          </b-form-row>
+        </b-form>
+      </b-row>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div data-cy="text">
-    Query Tool
-  </div>
+  <b-container fluid>
+    <b-row class="mx-4">
+      <h1 class="mt-2">
+        Cohort Definition Tool
+      </h1>
+      <hr class="mt-0 mb-4">
+    </b-row>
+    <query-form />
+  </b-container>
 </template>
 
 <script>
