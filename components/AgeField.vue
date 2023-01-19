080389b159f19ecc2ebbd98575fa12557dddf45b
rmanaem <rmanaem@protonmail.ch>
2023-01-19 15:00:46 -0500

    Added and implemented AgeField.vue component

---
+++ components/AgeField.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-form-row class="row">
+    <b-form-group class="col-md-6">
+      <div class="mb-3">
+        <label class="form-label">Min Age</label>
+        <b-form-input
+          v-model="minAge"
+          type="number"
+          step="any"
+          class="numberinput form-control"
+          @input="updateAge"
+        />
+      </div>
+    </b-form-group>
+    <b-form-group class="col-md-6">
+      <div class="mb-3">
+        <label class="form-label">Max Age</label>
+        <b-form-input
+          v-model="maxAge"
+          type="number"
+          step="any"
+          class="numberinput form-control"
+          @input="updateAge"
+        />
+      </div>
+    </b-form-group>
+  </b-form-row>
+</template>
+
+<script>
+export default {
+  emits: ['update-age'],
+  data() {
+    return {
+      minAge: null,
+      maxAge: null,
+    };
+  },
+  methods: {
+    updateAge() {
+      this.$emit('update-age', this.minAge, this.maxAge);
+    },
+  },
+
+};
+
+</script>
