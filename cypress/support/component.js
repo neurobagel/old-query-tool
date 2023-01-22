1ae9e147f5980a122743a66fe659d1d823435f87
rmanaem <rmanaem@protonmail.ch>
2023-01-22 15:30:04 -0500

    Imported bootstrapvue into cypress for component testing
    - Added bootstrap-vue.js to cypress/support

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -16,6 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
+// Import BootstrapVue
+import './bootstrap-vue';
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
