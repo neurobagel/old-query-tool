f52866a1f9268c63aeac2c7cb21befc5367bdfc7
rmanaem <rmanaem@protonmail.ch>
2023-01-26 15:21:05 -0500

    Implemented functionality of download results button

--- components/DownloadResultsButton.vue
+++ components/DownloadResultsButton.vue
@@ -10,6 +10,7 @@
       <b-button
         id="download-results-button"
         :disabled="downloads.length === 0"
+        @click="download"
       >
         <b-icon
           icon="download"
@@ -35,6 +36,46 @@ export default {
       default: () => [],
     },
   },
+  methods: {
+    generateCSVString() {
+      const data = [];
+      this.results.filter((res) => this.downloads.includes(res.dataset_name)).forEach((res) => {
+        res.subject_file_paths.forEach((path) => {
+          data.push({
+            'dataset ': res.dataset_name,
+            'number of matching subjects': res.num_matching_subjects,
+            'subject file paths': path,
+          });
+        });
+      });
 
+      const csvRows = [];
+      const headers = Object.keys(data[0]);
+
+      csvRows.push(headers.join(','));
+      data.forEach((row) => {
+        const values = headers.map((header) => {
+          const val = row[header];
+          return `"${val}"`;
+        });
+        csvRows.push(values.join(','));
+      });
+
+      return csvRows.join('\n');
+    },
+
+    download() {
+      const element = document.createElement('a');
+      element.setAttribute('href', `data:text/csv;charset=utf-8,
+      ${encodeURIComponent(this.generateCSVString())}`);
+      element.setAttribute('download', 'results.csv');
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+      document.body.removeChild(element);
+    },
+  },
 };
 </script>
