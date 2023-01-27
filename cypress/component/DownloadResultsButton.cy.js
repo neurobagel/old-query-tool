db7e09db91cab8d725cd448d8e80c33e551fb58a
rmanaem <rmanaem@protonmail.ch>
2023-01-26 23:57:01 -0500

    Implemented a test for DownloadResultsButton.vue
    - Added DownloadResultsButton.cy.js
    - Implemented test for displaying download results button and its functionality

---
+++ cypress/component/DownloadResultsButton.cy.js
@@ -0,0 +1,38 @@
+import DownloadResultsButton from '../../components/DownloadResultsButton.vue';
+
+const props = {
+  results: [
+    {
+      dataset: 'http://neurobagel.org/vocab/cool-dataset',
+      dataset_name: 'cool-dataset',
+      num_matching_subjects: 2,
+      subject_file_paths: ['cool-path', 'some-cool-path'],
+      image_modals: ['http://purl.org/nidash/nidm#T1Weighted', 'http://purl.org/nidash/nidm#T2Weighted'],
+    },
+    {
+      dataset: 'http://neurobagel.org/vocab/not-so-cool-dataset',
+      dataset_name: 'not-so-cool-dataset',
+      num_matching_subjects: 2,
+      subject_file_paths: ['not-so-cool-path', 'some-not-so-cool-path'],
+      image_modals: ['http://purl.org/nidash/nidm#FlowWeighted', 'http://purl.org/nidash/nidm#T1Weighted'],
+    },
+
+  ],
+  downloads: [],
+};
+
+describe('Download results button', () => {
+  it('Displays the disabled download results button', () => {
+    cy.mount(DownloadResultsButton, {
+      propsData: props,
+    });
+    cy.get("[data-cy='download-results-button']").should('be.visible').should('be.disabled');
+  });
+  it('Displays the enabled download results button', () => {
+    props.downloads = ['cool-dataset'];
+    cy.mount(DownloadResultsButton, {
+      propsData: props,
+    });
+    cy.get("[data-cy='download-results-button']").should('be.visible').should('not.be.disabled');
+  });
+});
