e403b8310f17bdfeaae756497e3b91632ceeaf8f
rmanaem <rmanaem@protonmail.ch>
2023-01-30 13:57:30 -0500

    Implemented SexField.vue component
    - Added SexField.vue
    - Implemented a vue select component and an event in SexField.vue

---
+++ components/SexField.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mb-3">
+    <label class="form-label">Sex</label>
+    <v-select
+      v-model="sex"
+      :options="Object.keys(options)"
+      @input="updateSex"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['update-sex'],
+  data() {
+    return {
+      options: {
+        All: null,
+        male: 'male',
+        female: 'female',
+        other: 'other',
+      },
+      sex: 'All',
+    };
+  },
+  methods: {
+    updateSex() {
+      this.$emit('update-sex', this.options[this.sex]);
+    },
+  },
+};
+</script>
