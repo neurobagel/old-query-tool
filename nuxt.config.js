bed58b244c1c2bed102790182dd7902f045fcd40
rmanaem <rmanaem@protonmail.ch>
2023-01-30 17:02:04 -0500

    Added categorical options as plugin
    - Added categorical-options.js
    - Registered categorical-options as a plugin in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
   plugins: [
     '@plugins/bootstrap-vue',
     '@plugins/vue-select',
+    '@plugins/categorical-options',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
