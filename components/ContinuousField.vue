3f16abffbfeb66e5092e49c51403b3ecb4f72a4d
rmanaem <rmanaem@protonmail.ch>
2023-02-01 22:21:59 -0500

    Implemented ContinuousField.vue component

---
+++ components/ContinuousField.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mb-3">
+    <label class="form-label">{{ name }}</label>
+    <b-form-input
+      v-model="input"
+      type="number"
+      step="any"
+      class="numberinput form-control"
+      min="0"
+      @input="updateField"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: null,
+    },
+  },
+  emits: ['update-continuous-field'],
+  data() {
+    return {
+      input: null,
+    };
+  },
+  methods: {
+    updateField() {
+      this.$emit('update-continuous-field', this.name, this.input);
+    },
+  },
+};
+</script>
