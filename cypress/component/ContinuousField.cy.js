8f9f9e33a4e2f4cf0d65058c5acdd04e65e17c77
rmanaem <rmanaem@protonmail.ch>
2023-02-10 01:26:26 -0500

    Updated ContinuousField.cy.js tests

--- cypress/component/ContinuousField.cy.js
+++ cypress/component/ContinuousField.cy.js
@@ -7,12 +7,12 @@ const props = {
 describe('Continuous field', () => {
   it('Displays an input field and a label for min age', () => {
     cy.mount(ContinuousField, { propsData: props });
-    cy.get('[data-cy="continuous-field-input"]').should('be.visible');
-    cy.get('[data-cy="continuous-field-label"]').should('be.visible');
+    cy.get('[data-cy="Min Age-continuous-field-input"]').should('be.visible');
+    cy.get('[data-cy="Min Age-continuous-field-input"]').should('be.visible');
   });
   it('Ignores when text is entered into the input field', () => {
     cy.mount(ContinuousField, { propsData: props });
-    cy.get('[data-cy="continuous-field-input"]').type('hello').should('have.value', '');
+    cy.get('[data-cy="Min Age-continuous-field-input"]').type('hello').should('have.value', '');
   });
   it('Emits update-continuous-field event when the value for min age and max age are updated', () => {
     cy.mount(ContinuousField, {
@@ -21,7 +21,7 @@ describe('Continuous field', () => {
         'update-continuous-field': cy.spy().as('spy'),
       },
     });
-    cy.get('[data-cy="continuous-field-input"]').type('7');
+    cy.get('[data-cy="Min Age-continuous-field-input"]').type('7');
     cy.get('@spy').should('have.been.calledWith', 'Min Age', '7');
   });
 });
