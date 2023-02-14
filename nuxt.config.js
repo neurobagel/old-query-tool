ba5273fb84b2dfa384c26882bba6868c5c0ed6ea
rmanaem <rmanaem@protonmail.ch>
2023-02-14 03:12:49 -0500

    Configured nuxt to use static target in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
+    target: 'static',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
