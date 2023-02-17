49c802327f86bd2e753f1e7b7d1d727b596c2eaa
rmanaem <rmanaem@protonmail.ch>
2023-02-17 16:16:17 -0500

    Added configurtion for cypress end to end tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   component: {
     devServer: {
-      framework: 'nuxt',
-      bundler: 'webpack',
+      framework: "nuxt",
+      bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
