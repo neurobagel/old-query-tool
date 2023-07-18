dea07a3204b60d6b998ac054dccaee2d93192e31
rmanaem <rmanaem@protonmail.ch>
2023-07-18 11:38:12 -0400

    Implemented a test for the bug

--- cypress/e2e/DatasetResultsTSV.cy.js
+++ cypress/e2e/DatasetResultsTSV.cy.js
@@ -19,4 +19,15 @@ describe('Dataset results TSV', () => {
     cy.get('[data-cy="download-dataset-level-results-button"]').click();
     cy.readFile('cypress/downloads/dataset-results.tsv').should('contain', 'some name');
   });
+  it('Removes the unwanted whitespace from the downloaded results files', () => {
+    cy.viewport(2000, 1000);
+    cy.intercept('query/?*', response).as('call');
+    cy.visit('/');
+    cy.get('[data-cy="submit-query"]').click();
+    cy.get('[data-cy="select-all"]').check();
+    cy.get('[data-cy="download-dataset-level-results-button"]').click();
+    cy.readFile('cypress/downloads/dataset-results.tsv').should('not.contain', '\n      DatasetID');
+    cy.get('[data-cy="download-participant-level-results-button"]').click();
+    cy.readFile('cypress/downloads/dataset-results.tsv').should('not.contain', '\n      DatasetID');
+  });
 });
