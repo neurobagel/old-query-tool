6b5d194622a076d4e582627b2848901d48410bc3
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2023-07-20 11:03:18 -0400

    [BUG] Removed the unwanted whitespace from results tsv files (#161)
    * Refactored `downloadResults` method
    
    The refactor was to resolve bug 151. It seems the bug was
    introduced because of the newline character between line 114
    and 115 in the `element.setAttribute`.
    
    * Implemented a test for the bug
    
    * Renamed `DatasetResultsTSV.cy.js` to `ResultsTSV.cy.js`
    
    * Changed the subtitle
    
    * Updated `Navbar` component test
    
    * Modified the test case to assert over matching a regex
    
    * Updated `Documentation` link in `Navbar` component

--- cypress/component/Navbar.cy.js
+++ cypress/component/Navbar.cy.js
@@ -5,7 +5,7 @@ describe('Navbar', () => {
     cy.mount(Navbar);
     cy.get('[data-cy="brand"]').should('be.visible');
     cy.get('[data-cy="brand"]').contains('Neurobagel Query');
-    cy.get('[data-cy="brand"]').contains('Define and find cohorts at the sample level');
+    cy.get('[data-cy="brand"]').contains('Define and find cohorts at the subject level');
     cy.get('[data-cy="brand"]').should('have.attr', 'href', 'https://www.neurobagel.org/');
     cy.get('[data-cy="version"]').should('be.visible');
     cy.get('[data-cy="version"]').contains('v0.1.0');
@@ -15,7 +15,7 @@ describe('Navbar', () => {
     cy.get('[data-cy="docs"]').should('be.visible');
     cy.get('[data-cy="docs"]').contains('Documentation');
     cy.get('[data-cy="docs"]').within(() => {
-      cy.get('a').should('have.attr', 'href', 'https://www.neurobagel.org/documentation/');
+      cy.get('a').should('have.attr', 'href', 'https://www.neurobagel.org/query_tool/');
     });
     cy.get('[data-cy="github"]').should('be.visible');
     cy.get('[data-cy="github"]').within(() => {
--- cypress/e2e/ResultsTSV.cy.js
+++ cypress/e2e/ResultsTSV.cy.js
@@ -9,7 +9,7 @@ const response = [
   },
 ];
 
-describe('Dataset results TSV', () => {
+describe('Results TSV', () => {
   it('Removes a newline character from a dataset name in the downloaded dataset-level results file', () => {
     cy.viewport(2000, 1000);
     cy.intercept('query/?*', response).as('call');
@@ -18,5 +18,20 @@ describe('Dataset results TSV', () => {
     cy.get('[data-cy="select-all"]').check();
     cy.get('[data-cy="download-dataset-level-results-button"]').click();
     cy.readFile('cypress/downloads/dataset-results.tsv').should('contain', 'some name');
+  });
+  it('Removes the unwanted whitespace from the downloaded results files', () => {
+    cy.viewport(2000, 1000);
+    cy.intercept('query/?*', response).as('call');
+    cy.visit('/');
+    cy.get('[data-cy="submit-query"]').click();
+    cy.get('[data-cy="select-all"]').check();
+    cy.get('[data-cy="download-dataset-level-results-button"]').click();
+    cy.readFile('cypress/downloads/dataset-results.tsv').then((fileContent) => {
+      expect(fileContent).to.match(/^DatasetID/);
+    });
+    cy.get('[data-cy="download-participant-level-results-button"]').click();
+    cy.readFile('cypress/downloads/participant-results.tsv').then((fileContent) => {
+      expect(fileContent).to.match(/^DatasetID/);
+    });
   });
 });
