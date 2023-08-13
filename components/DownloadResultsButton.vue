4da54d385f4cc65b796c52d7aaa334dad44ae54b
rmanaem <rmanaem@protonmail.ch>
2023-08-13 15:42:35 -0400

    Refactored `DownloadResultsButton` and `ResultsContainer` components
    - Moved `generateTSVString` and `downloadResults` methods from
    `DownloadResultsButton` to `ResultsContainer`
    - Removed `results` and `downloads` props in `DownloadResultsButton`
    - Added `downloadIsEmpty` prop in `DownloadResultsButton`
    - Removed `displayToolTip` method and moved the logic in line
    - Implemented `download-results` event in `DownloadResultsButton`

--- components/DownloadResultsButton.vue
+++ components/DownloadResultsButton.vue
@@ -1,13 +1,13 @@
 <template>
   <span
-    v-b-tooltip.hover.top="displayToolTip"
+    v-b-tooltip.hover.top="downloadsIsEmpty ? 'Select at least one dataset for download' : ''"
     :data-cy="`download-` + identifier + `-results-button-tooltip`"
   >
     <b-button
       class="nb-button"
-      :disabled="downloads.length === 0"
+      :disabled="downloadsIsEmpty"
       :data-cy="`download-` + identifier + `-results-button`"
-      @click="downloadResults(identifier)"
+      @click="$emit('download-results', identifier)"
     >
       <b-icon
         icon="download"
@@ -25,75 +25,11 @@ export default {
       type: String,
       default: 'participant-level',
     },
-    results: {
-      type: Array,
-      default: () => [],
-    },
-    downloads: {
-      type: Array,
-      default: () => [],
+    downloadsIsEmpty: {
+      type: Boolean,
+      default: true,
     },
   },
-  computed: {
-    displayToolTip() {
-      return this.downloads.length === 0 ? 'Select at least one dataset for download' : '';
-    },
-  },
-  methods: {
-    generateTSVString(buttonIdentifier) {
-      const tsvRows = [];
-      const datasets = this.results.filter((res) => this.downloads.includes(res.dataset_uuid));
-
-      if (buttonIdentifier === 'participant-level') {
-        const headers = ['DatasetID', 'SubjectID', 'Age', 'Sex', 'Diagnosis', 'Assessment', 'SessionID', 'SessionPath', 'NumSessions', 'Modality'].join('\t');
-        tsvRows.push(headers);
-
-        datasets.forEach((res) => {
-          res.subject_data.forEach((subject) => {
-            tsvRows.push([
-              res.dataset_uuid,
-              subject.sub_id,
-              subject.age,
-              subject.sex,
-              subject.diagnosis?.join(', '),
-              subject.assessment?.join(', '),
-              subject.session_id,
-              subject.session_file_path,
-              subject.num_sessions,
-              subject.image_modal?.join(', '),
-            ].join('\t'));
-          });
-        });
-      } else {
-        const headers = ['DatasetID', 'DatasetName', 'PortalURI', 'NumMatchingSubjects', 'AvailableImageModalities'].join('\t');
-        tsvRows.push(headers);
-
-        datasets.forEach((res) => {
-          tsvRows.push([
-            res.dataset_uuid,
-            res.dataset_name.replace('\n', ' '),
-            res.dataset_portal_uri,
-            res.num_matching_subjects,
-            res.image_modals?.join(', '),
-          ].join('\t'));
-        });
-      }
-
-      return tsvRows.join('\n');
-    },
-
-    downloadResults(buttonIdentifier) {
-      const element = document.createElement('a');
-      const encodedTSV = encodeURIComponent(this.generateTSVString(buttonIdentifier));
-      element.setAttribute('href', `data:text/tab-separated-values;charset=utf-8,${encodedTSV}`);
-      element.setAttribute('download', `${buttonIdentifier}-results.tsv`);
-
-      element.style.display = 'none';
-      document.body.appendChild(element);
-
-      element.click();
-      document.body.removeChild(element);
-    },
-  },
+  emits: ['download-results'],
 };
 </script>
--- components/ResultsContainer.vue
+++ components/ResultsContainer.vue
@@ -91,15 +91,15 @@
         <div class="download-buttons-div d-flex">
           <download-results-button
             identifier="participant-level"
-            :results="results"
-            :downloads="downloads"
+            :downloads-is-empty="downloads.length === 0"
             data-cy="download-participant-level-results"
+            @download-results="downloadResults"
           />
           <download-results-button
             identifier="dataset-level"
-            :results="results"
-            :downloads="downloads"
+            :downloads-is-empty="downloads.length === 0"
             data-cy="download-dataset-level-results"
+            @download-results="downloadResults"
           />
         </div>
       </b-row>
@@ -164,6 +164,60 @@ export default {
       } else {
         this.downloads = this.downloads.filter((item) => item !== datasetUuid);
       }
+    },
+    generateTSVString(buttonIdentifier) {
+      const tsvRows = [];
+      const datasets = this.results.filter((res) => this.downloads.includes(res.dataset_uuid));
+
+      if (buttonIdentifier === 'participant-level') {
+        const headers = ['DatasetID', 'SubjectID', 'Age', 'Sex', 'Diagnosis', 'Assessment', 'SessionID', 'SessionPath', 'NumSessions', 'Modality'].join('\t');
+        tsvRows.push(headers);
+
+        datasets.forEach((res) => {
+          res.subject_data.forEach((subject) => {
+            tsvRows.push([
+              res.dataset_uuid,
+              subject.sub_id,
+              subject.age,
+              subject.sex,
+              subject.diagnosis?.join(', '),
+              subject.assessment?.join(', '),
+              subject.session_id,
+              subject.session_file_path,
+              subject.num_sessions,
+              subject.image_modal?.join(', '),
+            ].join('\t'));
+          });
+        });
+      } else {
+        const headers = ['DatasetID', 'DatasetName', 'PortalURI', 'NumMatchingSubjects', 'AvailableImageModalities'].join('\t');
+        tsvRows.push(headers);
+
+        datasets.forEach((res) => {
+          tsvRows.push([
+            res.dataset_uuid,
+            res.dataset_name.replace('\n', ' '),
+            res.dataset_portal_uri,
+            res.num_matching_subjects,
+            res.image_modals?.join(', '),
+          ].join('\t'));
+        });
+      }
+
+      return tsvRows.join('\n');
+    },
+
+    downloadResults(buttonIdentifier) {
+      const element = document.createElement('a');
+      const encodedTSV = encodeURIComponent(this.generateTSVString(buttonIdentifier));
+      element.setAttribute('href', `data:text/tab-separated-values;charset=utf-8,${encodedTSV}`);
+      element.setAttribute('download', `${buttonIdentifier}-results.tsv`);
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+      document.body.removeChild(element);
     },
   },
 };
