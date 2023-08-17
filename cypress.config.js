1ae4351d373cbdd366ae2addadbe91bc09d81437
rmanaem <rmanaem@protonmail.ch>
2023-08-17 16:00:55 -0400

    Set the default viewport for cypres e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,8 @@ module.exports = defineConfig({
   },
 
   e2e: {
+    viewportWidth: 2000,
+    viewportHeight: 1000,
     baseUrl: "http://localhost:3000",
     
     setupNodeEvents(on, config) {
