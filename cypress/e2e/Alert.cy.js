fb5b8d618f080e71972ef6e605a86ab28e990c5e
rmanaem <rmanaem@protonmail.ch>
2023-12-05 13:26:15 -0500

    Updated test

--- cypress/e2e/Alert.cy.js
+++ cypress/e2e/Alert.cy.js
@@ -1,9 +1,9 @@
 describe('Alert', () => {
   it('Correctly displays and dismisses the alert', () => {
-    cy.visit('/');
-    cy.get('[data-cy="alert"]').should('be.visible').should('contain', ' The OpenNeuro node is being actively annotated at the participant');
     cy.visit('/?node=Parkinson\'s Progression Markers Initiative');
     cy.get('[data-cy="alert"]').should('not.exist');
+    cy.visit('/');
+    cy.get('[data-cy="alert"]').should('be.visible').should('contain', ' The OpenNeuro node is being actively annotated at the participant');
     cy.get('[data-cy="node-field"]').type('OpenNeuro{enter}');
     cy.get('[data-cy="alert"]').should('be.visible');
     cy.get('[data-cy="dismiss-alert"]').click();
