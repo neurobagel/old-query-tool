1c52b0cd3cfe160c3dcb2778e3167280f38f8c36
rmanaem <rmanaem@protonmail.ch>
2023-12-19 13:55:05 -0500

    Updated tests

--- cypress/e2e/APIRequest.cy.js
+++ cypress/e2e/APIRequest.cy.js
@@ -9,6 +9,10 @@ describe('API request', () => {
     cy.visit('/?node=OpenNeuro');
     cy.get('[data-cy="Min Age-continuous-field-input"]').type('10');
     cy.get('[data-cy="Max Age-continuous-field-input"]').type('30');
+    // We need to wait for the fetch to complete and populate the
+    // dropdown with nodes and selecting OpenNeuro before making the request
+    // eslint-disable-next-line
+    cy.wait(1000);
     cy.get('[data-cy="submit-query"]').click();
     cy.wait('@call').its('request.url').should('contains', '&min_age=10&max_age=30');
   });
--- cypress/e2e/Alert.cy.js
+++ cypress/e2e/Alert.cy.js
@@ -4,8 +4,15 @@ describe('Alert', () => {
     cy.get('[data-cy="openneuro-alert"]').should('be.visible').should('contain', ' The OpenNeuro node is being actively annotated at the participant');
     cy.visit('/?node=Parkinson\'s Progression Markers Initiative');
     cy.get('[data-cy="openneuro-alert"]').should('not.exist');
+    // We need to wait for the fetch to complete and populate the
+    // dropdown with nodes before searching for OpenNeuro
+    // eslint-disable-next-line
+    cy.wait(1000);
     cy.get('[data-cy="node-field"]').type('OpenNeuro{enter}');
     cy.get('[data-cy="openneuro-alert"]').should('be.visible');
+    // TODO: remove once the alert is fixed
+    // eslint-disable-next-line
+    cy.wait(2000);
     cy.get('[data-cy="dismiss-openenuro-alert"]').click();
     cy.get('[data-cy="openneuro-alert"]').should('not.exist');
   });
