d2f611837a7b3dda76e56ed07276a523c6afd970
rmanaem <rmanaem@protonmail.ch>
2023-12-18 22:58:33 -0500

    Moved `static` from header to the top level

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,8 +19,8 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
-    target: 'static',
   },
+  target: 'static',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
