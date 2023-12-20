c296d1740631761b06f4b0db24ffc66ca9b2f4d8
rmanaem <rmanaem@protonmail.ch>
2023-12-20 13:51:58 -0500

    Refactored tests
    to intercept the request to `nodes` endpoint instead and wait
    on the response instead of waiting for a specific amount of time

--- cypress/e2e/APIRequest.cy.js
+++ cypress/e2e/APIRequest.cy.js
@@ -4,15 +4,18 @@ describe('API request', () => {
   it('Intercepts the request sent to the API and asserts over the request url', () => {
     cy.intercept({
       method: 'GET',
-      url: 'query/?*',
+      url: '**/query/?*',
     }, mixedResponse).as('call');
+    cy.intercept({
+      method: 'GET',
+      url: '**/nodes/',
+    }).as('getNodes');
     cy.visit('/?node=OpenNeuro');
-    cy.get('[data-cy="Min Age-continuous-field-input"]').type('10');
-    cy.get('[data-cy="Max Age-continuous-field-input"]').type('30');
     // We need to wait for the fetch to complete and populate the
     // dropdown with nodes and selecting OpenNeuro before making the request
-    // eslint-disable-next-line
-    cy.wait(1000);
+    cy.wait('@getNodes');
+    cy.get('[data-cy="Min Age-continuous-field-input"]').type('10');
+    cy.get('[data-cy="Max Age-continuous-field-input"]').type('30');
     cy.get('[data-cy="submit-query"]').click();
     cy.wait('@call').its('request.url').should('contains', '&min_age=10&max_age=30');
   });
